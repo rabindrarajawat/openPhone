16923c96d14c68aa9ce24526cbcc5e93f9ce894d
rupali <rupali.solanki@imentus.com>
2024-07-17 21:01:53 +0530

    correct database with env file

--- backend/src/config/datasource.ts
+++ backend/src/config/datasource.ts
@@ -1,18 +1,25 @@
- import { DataSource } from "typeorm"
-import {  OpenPhoneEvent } from "../entities/open-phone-event.entity"
-import { OpenPhoneEventType } from "../entities/open-phone-event-type.entity"
-import { OpenPhoneEventDirection } from "../entities/open-phone-event-direction.entity"
-import { Address } from "../entities/address.entity"
-import { AuctionEvent } from "../entities/auction-event.entity"
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+import { OpenPhoneEvent } from '../entities/open-phone-event.entity';
+import { OpenPhoneEventType } from '../entities/open-phone-event-type.entity';
+import { OpenPhoneEventDirection } from '../entities/open-phone-event-direction.entity';
+import { Address } from '../entities/address.entity';
+import { AuctionEvent } from '../entities/auction-event.entity';
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "openphone",
-    entities: [OpenPhoneEvent,OpenPhoneEventType,OpenPhoneEventDirection,Address,AuctionEvent],
-    migrations: ["src/migrations/**/*.ts"],
-    synchronize: false,
-})
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [
+        OpenPhoneEvent,
+        OpenPhoneEventType,
+        OpenPhoneEventDirection,
+        Address,
+        AuctionEvent
+    ],
+    migrations: ['src/migrations/**/*.ts'],
+    synchronize: process.env.DB_SYNC === 'true',
+});
