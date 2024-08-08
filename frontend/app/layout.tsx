e40fa86810d6e7fd06a273ee70e27e46839103a6
rupali <rupali.solanki@imentus.com>
2024-08-08 13:40:45 +0530

    latest changes in ui

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
 
 const inter = Inter({ subsets: ["latin"] });
 
