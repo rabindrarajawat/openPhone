f74bac8574f93cab7dcda51f5a448ecfa7a0c7ec
rup-sol <rupali.solanki@imentus.com>
2024-10-03 18:37:10 +0530

    changes in sidenavbar and converstion

--- frontend/app/SideNavbar/sideNavbar.tsx
+++ frontend/app/SideNavbar/sideNavbar.tsx
@@ -1,42 +1,28 @@
-"use client";
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
+import styles from '../SideNavbar/SideNavbar.module.css';
+import Image from 'next/image';
+import Link from 'next/link'; // Import Link from Next.js
 
-import { useRouter } from "next/navigation";
-import styles from "./SideNavbar.module.css"; 
-import "bootstrap-icons/font/bootstrap-icons.css";
-
-const SideBar = () => {
-  const router = useRouter();
-
+function Sidebar() {
   const handleLogout = () => {
-    console.log("Logout clicked");
+    console.log("Logout clicked"); // Debugging line
     localStorage.removeItem("authToken");
-    window.location.href = "/";
+    window.location.href = "/"
   };
-
-
-
   return (
     <>
-      <ul className={styles.sidebar}>
-        <div>
-          <div className={styles.nav}>
-            <Link href="/Dashboard">
+      <nav className={`navbar  position-fixed vh-100 p-2 d-flex flex-column ${styles.bgCustom}`}>
+        <div className="d-flex flex-column justify-content-center align-items-center flex-grow-1 mt-4">
+        <Link href="/Dashboard">
               <Image
                 src="/upicon.svg"
                 alt="Logo"
-                className={`${styles.logo2}`}
+                className=""
                 width={50}
                 height={50}
               />
               <p className={`${styles.dash}`}>Dashboard</p>
             </Link>
-          </div>
-        </div>
-        <div>
-          <div className={styles.nav}>
+
             <Link href={'/conversationmapping'}>
             
             <Image
@@ -46,17 +32,19 @@ const SideBar = () => {
               width={50}
               height={50}
             />
-            <p className={styles.dash}>Map Address</p>
+            <p className="">Map Address</p>
             </Link>
-          </div>
         </div>
 
-        <div className={styles.log}>
-          <div onClick={handleLogout}>Log out</div>
-        </div>
-      </ul>
+        <div className="d-flex flex-column justify-content-end align-items-end flex-grow-1 mb-4 fs-6">
+  <button className="btn btn-link p-0 text-decoration-none" onClick={handleLogout}>
+    Logout
+  </button>
+</div>
+
+      </nav>
     </>
   );
-};
+}
 
-export default SideBar;
+export default Sidebar;
